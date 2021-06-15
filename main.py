4baa99f653cd8ba8f184c64ee585206ca50fd90e
wataruyamamoto127 <85893778+wataruyamamoto127@users.noreply.github.com>
2021-06-16 00:32:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(SECRET)
