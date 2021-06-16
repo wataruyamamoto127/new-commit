8bfcd4217badc5265887ab8c0f9ec81a2a3b423e
wataruyamamoto127 <85893778+wataruyamamoto127@users.noreply.github.com>
2021-06-16 17:45:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,7 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
